9ee5bce078be3ea21a2098eead2e2ab30dba185b
서새찬 <chan4760@gmail.com>
2024-03-28 18:56:32 +0900

    feature: OutputView 정의

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -4,6 +4,9 @@ import static view.OutputMessage.CAR_RACING_RESULT_HEADER;
 import static view.OutputMessage.ENTER_ATTEMPT_COUNT;
 import static view.OutputMessage.ENTER_CAR_NAMES;
 
+import java.util.List;
+import model.entity.Car;
+
 public class OutputView {
   public static void printEnterCarNamesOutputMessage(){
     System.out.println(ENTER_CAR_NAMES);
@@ -19,5 +22,21 @@ public class OutputView {
 
   public static void printErrorMessage(String message) {
     System.out.println(message);
+  }
+
+  public static void printRaceResult(List<Car> cars) {
+    printCarRacingResultHeader();
+    for (Car car : cars) {
+      System.out.println(car.getCarName() + " : " + "-".repeat(car.getMovedDistance()));
+    }
+    System.out.println();
+  }
+
+  public static void printWinners(List<Car> winners){
+    String winnerNames = winners.stream()
+        .map(Car::getCarName)
+        .reduce((name1, name2) -> name1 + ", " + name2)
+        .orElse("");
+    System.out.println(winnerNames + "가 최종 우승했습니다.");
   }
 }
