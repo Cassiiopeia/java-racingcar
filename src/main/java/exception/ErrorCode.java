d78a1d27a67ee6527946faf1fdb136e30393f85c
서새찬 <chan4760@gmail.com>
2024-03-28 18:58:12 +0900

    feature: INVALID_RANDOM_VALUE 정의

--- src/main/java/exception/ErrorCode.java
+++ src/main/java/exception/ErrorCode.java
@@ -3,6 +3,7 @@ package exception;
 public enum ErrorCode {
   EXAMPLE_ERROR_MESSAGE("예시 에러 메시지 입니다."),
   INVALID_CAR_NAME_LENGTH("이름 길이는 1~5자여야합니다."),
+  INVALID_RANDOM_VALUE("랜던값은 0~9 인 숫자여야합니다."),
   INVALID_ATTEMPT_COUNT_INPUT("");
   private static final String ERROR_HEADER = "[ERROR] : ";
   private final String message;
